47df1e6efabcb19b3ff84ac48b07607a418cfd39
Mikhail <mefisto40000x@gmail.com>
2020-05-10 21:07:22 +0300

    Create project

---
+++ SmartHouseBalance/SmartHouseBalance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartHouseBalance
+//
+//  Created by Михаил Беленко on 10.05.2020.
+//  Copyright © 2020 Михаил Беленко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
